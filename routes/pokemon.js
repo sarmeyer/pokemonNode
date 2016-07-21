9b79a8cadd130725757a5b12498b92be69a6249e
Sarah <sarahmichellemeyer@gmail.com>
2016-07-21 14:29:38 -0600

    user is redirected to show page after editing a pokomon

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -7,15 +7,15 @@ router.get('/', function(req, res, next) {
   Pokemon.all().then(function(pokemon){
     res.render('pokemon/index', {pokemon: pokemon.rows});
   })
-});
+})
 router.get('/new', function(req,res,next){
   res.render('pokemon/new')
-});
+})
 router.post('/', function(req, res, next) {
   Pokemon.create(req.body).then(function(){
     res.redirect('/pokemon');
   })
-});
+})
 router.get('/:id', function(req,res,next){
   Pokemon.show(req.params.id).then(function(pokemon){
     Pokemon.displayTrainer(pokemon.rows[0].trainer_id).then(function(trainer){
@@ -25,10 +25,19 @@ router.get('/:id', function(req,res,next){
 })
 router.get('/:id/edit', function(req,res,next){
   Pokemon.show(req.params.id).then(function(pokemon){
+    Pokemon.displayTrainer(pokemon.rows[0].trainer_id).then(function(trainer){
     Pokemon.showTrainers().then(function(trainers){
-      console.log('*************');
-      console.log(trainers.rows);
-      res.render('pokemon/edit', {pokemon: pokemon.rows[0], trainers: trainers.rows})
+      res.render('pokemon/edit', {pokemon: pokemon.rows[0], trainer: trainer.rows[0], trainers: trainers.rows})
+      })
+    })
+  })
+})
+router.post('/:id', function(req, res, next) {
+  Pokemon.edit(req.params.id, req.body).then(function(){
+    Pokemon.show(req.params.id).then(function(pokemon){
+      Pokemon.displayTrainer(pokemon.rows[0].trainer_id).then(function(trainer){
+        res.render('pokemon/show', {pokemon: pokemon.rows[0], trainer: trainer.rows[0]})
+      })
     })
   })
 })
