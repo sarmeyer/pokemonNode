f7072c31f20d1bd4187aab90f05df06dca5b1886
Sarah <sarahmichellemeyer@gmail.com>
2016-07-21 09:59:19 -0600

    changing views to jade

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
+var Pokemon = require('../lib/queries');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.render('pokemon/index', {passedInData: "abc"});
+  Pokemon.all().then(function(pokemon){
+    console.log(pokemon.rows);
+    res.render('pokemon/index', {pokemon: pokemon.rows});
+  })
 });
 
 module.exports = router;
