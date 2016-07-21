83faa274b79d9455a3a1d2ded966ef00713dba36
Sarah <sarahmichellemeyer@gmail.com>
2016-07-21 10:44:28 -0600

    user story 3 complete

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -5,8 +5,12 @@ var Pokemon = require('../lib/queries');
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   Pokemon.all().then(function(pokemon){
-    console.log(pokemon.rows);
     res.render('pokemon/index', {pokemon: pokemon.rows});
+  })
+});
+router.get('/:id', function(req, res, next) {
+  Pokemon.show(req.params.id).then(function(pokemon){
+    res.render('pokemon/', {pokemon: pokemon.rows[0]});
   })
 });
 
