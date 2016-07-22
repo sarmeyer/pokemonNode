70d11c5cbed8b694b8091b4fbfa63ad8896019b2
Sarah <sarahmichellemeyer@gmail.com>
2016-07-21 19:13:57 -0600

    user can see one trainer on a trainer show page

--- routes/trainers.js
+++ routes/trainers.js
@@ -7,5 +7,12 @@ router.get('/', function(req, res, next) {
     res.render('trainers/index', {trainers: trainers.rows});
   })
 });
+router.get('/:id', function(req, res, next) {
+  Trainer.displayTrainer(req.params.id).then(function(trainer){
+    Trainer.trainerPoke(req.params.id).then(function(pokemon){
+      res.render('trainers/show', {trainer: trainer.rows[0], pokemon: pokemon.rows});
+    })
+  })
+});
 
 module.exports = router;
